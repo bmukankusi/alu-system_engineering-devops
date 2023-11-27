5109495e4a4714ddebaf14e0e036fd28872ec978
bmukankusi <b.mukankusi@alustudent.com>
2023-11-27 10:35:32 -0800

    100-puppet_ssh_config.pp

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 #Client configuration using Puppet
 
-file { 'etc/ssh/ssh_config':
+file { '/etc/ssh/ssh_config':
         ensure => present,
 
 content =>"
 
-          #SSH client config
+          #SSH client configuration
           host*
           IdentityFile ~/.ssh/school
           PasswordAuthentification no
-          ",
+          "
 }
